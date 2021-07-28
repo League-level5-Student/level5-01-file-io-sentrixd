b71ea3f2a009c67375d056cfb608484e64df8cc7
sen_trix <wintergraham10@gmail.com>
2021-07-27 19:36:55 -0700

    Finished

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,5 +1,21 @@
 package _03_To_Do_List;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class ToDoList {
 	/*
 	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
@@ -22,7 +38,122 @@ public class ToDoList {
 	 * When the program starts, it should automatically load the last saved file into the list. 
 	 */
 	
+	public static ArrayList<String> epiclist = new ArrayList<String>();
+	
 	public static void main(String[] args) {
+		JFrame frame = new JFrame("Todo list");
+		JPanel panel = new JPanel();
 		
+		frame.add(panel);
+		
+		panel.setLayout(new FlowLayout());
+		
+		JButton addTask = new JButton();
+		JButton viewTasks = new JButton();
+		JButton removeTask = new JButton();
+		JButton saveList = new JButton();
+		JButton loadList = new JButton();
+		
+		addTask.setText("Add Task");
+		viewTasks.setText("View Tasks");
+		removeTask.setText("Remove task");
+		saveList.setText("Save list");
+		loadList.setText("Load List");
+		
+		addTask.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String input = new JOptionPane().showInputDialog("What to add to the list:");
+				input = input + "";
+				
+				epiclist.add(input);
+			}
+		});
+		
+		viewTasks.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				load();
+				String output = ""; 
+				for (String s:epiclist) {
+					output = output + s + "\n";
+				}
+				System.out.println(output);
+			}
+		});
+		
+		removeTask.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String input = new JOptionPane().showInputDialog("Task to remove:");
+				epiclist.remove(input);
+			}
+		});
+		
+		saveList.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				save();
+			}
+		});
+		
+		loadList.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				load();
+				String output = ""; 
+				for (String s:epiclist) {
+					output = output + s + "\n";
+				}
+				System.out.println(output);
+			}
+		});
+		
+		panel.add(addTask);
+		panel.add(viewTasks);
+		panel.add(removeTask);
+		panel.add(saveList);
+		panel.add(loadList);
+		
+		frame.setSize(200, 300);  
+        frame.setLocationRelativeTo(null);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        frame.setVisible(true);  
+	}
+	
+	private static void save() {
+		try {
+			FileWriter fw = new FileWriter("src/_03_To_Do_List/save.txt");
+			
+			for (String s: epiclist) {
+				fw.write(s+"\n");
+			}
+				
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void load() {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/_03_To_Do_List/save.txt"));
+			String line = br.readLine();
+			
+			epiclist.clear();
+			
+			while(line != null){
+				epiclist.add(line);
+				line = br.readLine();
+			}
+			
+			br.close();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
